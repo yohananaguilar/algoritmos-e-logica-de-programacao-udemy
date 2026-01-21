4f7739e9dcf261e001c3da4200bf2fd094f7f7be
y0shyy <yochagamer1@gmail.com>
2026-01-20 22:45:25 -0300

    implementa declaração de variáveis e saída de dados em Python
    Adiciona script inicial em Python para demonstrar fundamentos da linguagem.
    O código exemplifica:
    - Declaração de variáveis com tipagem explícita (type hinting).
    - Atribuição de valores para tipos inteiros, reais (float) e strings.
    - Exibição dos dados no console utilizando a função print.

---
+++ Python/Estruturas Sequenciais/teste.py
@@ -0,0 +1,17 @@
+idade: int
+salario: float
+altura: float
+genero: str
+nome: str
+
+idade = 25
+salario = 1610.03
+altura = 1.90
+genero = "M"
+nome = "João Silva Guimaraes"
+
+print(idade)
+print(salario)
+print(altura)
+print(genero)
+print(nome)
