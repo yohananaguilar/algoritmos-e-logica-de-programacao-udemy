ab58c50a5519518cc0e170429a9be45d128a7c2a
y0shyy <yochagamer1@gmail.com>
2026-01-21 22:02:33 -0300

    adiciona exercícios de entrada e processamento em Python
    Inclui novos scripts para prática de sintaxe Python:
    - Geometria: Cálculos de retângulo utilizando a biblioteca math.
    - Lógica: Cálculo de média de idades com formatação de strings.
    - Fundamentos: Exemplos de input/output com tipagem explícita (type hinting) e f-strings.

---
+++ Python/Estruturas Sequenciais/retangulo.py
@@ -0,0 +1,14 @@
+import math
+
+base: float; altura: float; area: float; perimetro: float; diagonal: float
+
+base = float(input("Base do retangulo: "))
+altura = float(input("Altura do retangulo: "))
+
+area = base * altura
+perimetro = (2 * base) + (2 * altura)
+diagonal = math.sqrt((base ** 2) + (altura ** 2))
+
+print(f"AREA = {area:.4f}")
+print(f"PERIMETRO = {perimetro:.4f}")
+print(f"DIAGONAL = {diagonal:.4f}")
