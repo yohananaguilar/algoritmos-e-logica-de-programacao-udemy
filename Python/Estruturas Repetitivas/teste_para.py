af835aafeeb11707359a82c65da230af43da94fb
y0shyy <yochagamer1@gmail.com>
2026-01-21 22:08:38 -0300

    adiciona bateria de exercícios de sintaxe e estruturas em Python
    Inclui scripts cobrindo estruturas fundamentais da linguagem:
    - Controle de Fluxo: Condicionais (if/elif) e loops (for/while).
    - Estruturas de Dados: Vetores e Matrizes implementados com Listas e Listas Aninhadas.
    - Utilização de type hinting e f-strings para formatação de saída.

---
+++ Python/Estruturas Repetitivas/teste_para.py
@@ -0,0 +1,11 @@
+x: int
+soma: int
+
+N = int(input("Quantos numeros serao digitados? "))
+soma = 0
+
+for i in range(0, N):
+    x = int(input("Digite um numero: "))
+    soma = soma + x
+
+print("SOMA = ", soma)
